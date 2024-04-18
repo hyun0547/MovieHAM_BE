e440f4bce112e78138e7ecc3fa5c76c4ccd498cc
hyun0547 <hyun0547@gmail.com>
2024-04-18 15:20:59 +0900

    code cleanup

--- src/main/java/com/movieHam/movie/repository/MovieCustomRepositoryImpl.java
+++ src/main/java/com/movieHam/movie/repository/MovieCustomRepositoryImpl.java
@@ -3,7 +3,6 @@ package com.movieHam.movie.repository;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
--- src/main/java/com/movieHam/movie/service/MovieServiceImpl.java
+++ src/main/java/com/movieHam/movie/service/MovieServiceImpl.java
@@ -2,7 +2,6 @@ package com.movieHam.movie.service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Date;
 
 import org.springframework.stereotype.Service;
 
